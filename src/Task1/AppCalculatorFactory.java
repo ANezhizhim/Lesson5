f8551777dee00237b706e456b8e60c139f310801
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-24 10:47:44 +0300

    Задание 1 урок 5

--- src/Task1/AppCalculatorFactory.java
+++ src/Task1/AppCalculatorFactory.java
@@ -1,5 +1,4 @@
 package Task1;
-
 import java.util.Scanner;
 
 public class AppCalculatorFactory {
@@ -11,75 +10,23 @@ public class AppCalculatorFactory {
         logFactories[1] = new DbLoggerFactory();
         logFactories[2] = new ConsoleLoggerFactory();
 
-
         for (int i = 0; i < 3; i++) {
             loggers[i] = logFactories[i].createLogger();
         }
 
-        calculator.setLog(loggers);
-
-        int intLen, i, int_c, intCodeOper;
-        char c;
-        String strOperand;
-        double dblOperand = 0, dblResult = 0;
-        boolean boolExecuteOperation;//Признак необходимости выполнения арифметической операции
-        String strExpression;
+        calculator.setLog(loggers);//Привязываем  логгеры к калькулятору
+        String strExpression;//Строка с арифметическим выражением
 
         do {
             System.out.println("Введите выражение и нажмите [Enter] , для выхода 'ex': ");
             Scanner exprScan = new Scanner(System.in);
             strExpression = exprScan.nextLine();//Блок try-catch не имеет смысла т.к. к преобразованию в тип double подлежат только проверенные в теле программы символы (0-9, ".", арифметические знаки)
-//Определяем длину выражения
-            intLen = strExpression.length();
-//парсим строку
-            dblResult = 0;//обнуляем результат предыдущих вычислений
-            strOperand = "0";//обнуляем строчное представление последнего операнда
-            boolExecuteOperation = false;//Признак необходимости выполнения арифметической операции
-            intCodeOper = 43;//Код операции , по умолчанию "+";
-            i = -1;
-            while (++i < intLen) {
-                c = strExpression.charAt(i);
-                int_c = (int) c;
-                if ((int_c == 46) || (int_c > 47) && (int_c < 58)) {
-                    strOperand += c;// конкатенируем числовую строку операнда
-                } else if ((int_c == 42) || (int_c == 43) || (int_c == 45) || (int_c == 47)) { //если попадаем на знаки операций , то выполняем операцию с последним операндом
-                    boolExecuteOperation = true;
-                }
-
-                if (i == (intLen - 1)) {
-                    boolExecuteOperation = true;//если попадаем на конец строки, то выполняем операцию с последним операндом
-                }
-
-                if (boolExecuteOperation) {
-                    dblOperand = Double.parseDouble(strOperand);//переводим строку в тип Double
-                    switch (intCodeOper) {
-                        case 42:
-                            dblResult = calculator.multiplication(dblResult, dblOperand);
-                            break;
-                        case 43:
-                            dblResult = calculator.addition(dblResult, dblOperand);
-                            break;
-                        case 45:
-                            dblResult = calculator.subtraction(dblResult, dblOperand);
-                            break;
-                        case 47:
-                            dblResult = calculator.division(dblResult, dblOperand);
-                            break;
-
-                    }
-                    boolExecuteOperation = false;
-                    strOperand = "0";
-                    intCodeOper = int_c;
-                }
+            if (!strExpression.equals("ex")) {
+                System.out.println("Ответ: " + calculator.execute(strExpression));
             }
-            if (!strExpression.equals("ex")) System.out.println("Ответ: " + dblResult);
         } while (!strExpression.equals("ex"));
-
         System.out.println("До свидания!");
-
-
     }
-
 }
 /*
 
--- src/Task1/Calculator.java
+++ src/Task1/Calculator.java
@@ -1,39 +1,99 @@
 package Task1;
 
 public class Calculator {
-
     public Calculator() {
-
     }
-
-    private Logger []log =new Logger [3];
+    private Logger[] log = new Logger[3];
+    private String clcStr;
 
     public void setLog(Logger... logg) {
         log = logg;
-
-
     }
 
+    public double execute(String calcString) {
+        clcStr = calcString;
 
-    public double addition(double a, double b) {
-        for (Logger lg:
-                log) {
-           lg.log ("Addition");
+        int intLen = clcStr.length() - 1,//длина введенной строки с арифметическим выражением
+                i = -1,//счетчик
+                int_c,//код символа в введенной строке , распознается как операнд или его часть или знак операции(+,-,*,/)
+                intCodeOper=43;//Код операции , по умолчанию "+";
+        char c;//символ в строке с арифметическим выражением
+        String strOperand="0" ;//строчное представление последнего операнда
+        double dblOperand = 0, dblResult = 0;// числовое предсавление операнда и результата
+        boolean boolExecuteOperation = false;//Признак необходимости выполнения арифметической операции
+
+//парсим строку
+        while (++i <= intLen) {
+            c = clcStr.charAt(i);
+            int_c = (int) c;
+            if ((int_c == 46) || (int_c > 47) && (int_c < 58)) {
+                strOperand += c;// конкатенируем числовую строку операнда
+            } else if ((int_c == 42) || (int_c == 43) || (int_c == 45) || (int_c == 47)) { //если попадаем на знаки операций , то выполняем операцию с последним операндом
+                boolExecuteOperation = true;//признак необходимости выполнить арифметическую операцию
+            }
+
+            if (i == intLen) {
+                boolExecuteOperation = true;//если попадаем на конец строки, то выполняем операцию с последним операндом
+            }
+
+            if (boolExecuteOperation) {
+                dblOperand = Double.parseDouble(strOperand);//переводим строку в тип Double
+                switch (intCodeOper) {
+                    case 42:
+                        dblResult = this.multiplication(dblResult, dblOperand);
+                        break;
+                    case 43:
+                        dblResult = this.addition(dblResult, dblOperand);
+                        break;
+                    case 45:
+                        dblResult = this.subtraction(dblResult, dblOperand);
+                        break;
+                    case 47:
+                        dblResult = this.division(dblResult, dblOperand);
+                        break;
+                }
+                boolExecuteOperation = false;//сбрасываем признак необходимости выполнения арифметической операции
+                strOperand = "0";//Обнуляем строковое значение операнда
+                intCodeOper = int_c;//код последнего символа запоминаем для выполнения следующец операции
+            }
         }
-        //log[0].log("Addition");
-        return a + b;
-
+        return dblResult;
     }
 
-    public double subtraction(double a, double b) {
-        return a - b;
+    private double addition(double a, double b) {
+        double ret = a + b;
+        for (Logger lg :
+                log) {
+            lg.log("Addition result: " + ret);
+        }
+        return ret;
     }
 
-    public double multiplication(double a, double b) {
-        return a * b;
+    private double subtraction(double a, double b) {
+        double ret = a - b;
+        for (Logger lg :
+                log) {
+            lg.log("subtraction result: " + ret);
+        }
+        return ret;
     }
 
-    public double division(double a, double b) {
-        return a / b;
+    private double multiplication(double a, double b) {
+        double ret = a * b;
+        for (Logger lg :
+                log) {
+            lg.log("multiplication result: " + ret);
+        }
+        return ret;
+    }
+
+    private double division(double a, double b) {
+
+        double ret = a / b;
+        for (Logger lg :
+                log) {
+            lg.log("division result: " + ret);
+        }
+        return ret;
     }
 }
